cd036d2c915c861fdd6594760204ebb055b018c9
gaetanof <franciscogaetano1@gmail.com>
2024-07-28 23:30:45 -0300

    started new project with customizations

---
+++ frontegg-hosted-example/src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { FronteggProvider } from '@frontegg/react';
+
+const contextOptions = {
+  baseUrl: import.meta.env.VITE_FRONTEGG_BASE_URL,
+  clientId: import.meta.env.VITE_FRONTEGG_CLIENT_ID,
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <FronteggProvider contextOptions={contextOptions} hostedLoginBox={true}>
+      <App />
+    </FronteggProvider>
+  </React.StrictMode>
+);
